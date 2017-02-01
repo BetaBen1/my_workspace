b2e49ea9b7632894aaf1564185d766ae64a7c0b8
BenHardin <bbhardin1@gmail.com>
2017-02-01 12:12:38 -0500

    Updates

--- Inventory/src/com/betaben/inventory/Main.java
+++ Inventory/src/com/betaben/inventory/Main.java
@@ -3,6 +3,7 @@ package com.betaben.inventory;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
@@ -17,9 +18,14 @@ import javax.swing.JTextField;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JRadioButton;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
 
 public class Main extends JFrame {
 
+	static Main frame;
+	
 	private JPanel contentPane;
 
 	JPanel scrollPanel;
@@ -47,7 +53,7 @@ public class Main extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Main frame = new Main();
+					frame = new Main();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -65,55 +71,14 @@ public class Main extends JFrame {
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		
-		scrollPanel = new JPanel();
-		scrollPanel.setPreferredSize(new Dimension(141, 1000));
-		scrollPanel.setLayout(null);
-		
-		scrollPane = new JScrollPane();
-		scrollPane.setBounds(292, 25, 141, 247);
-		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-		scrollPane.setViewportView(scrollPanel);
-		contentPane.add(scrollPane);
-		
-		lblInventory = new JLabel("Inventory:");
-		lblInventory.setBounds(292, 6, 141, 16);
-		contentPane.add(lblInventory);
-		
-		nameTextField = new JTextField();
-		nameTextField.setBounds(6, 25, 274, 26);
-		contentPane.add(nameTextField);
-		nameTextField.setColumns(10);
-		
-		priceTextField = new JTextField();
-		priceTextField.setBounds(6, 91, 274, 26);
-		contentPane.add(priceTextField);
-		priceTextField.setColumns(10);
-		
-		lblItemName = new JLabel("Item Name:");
-		lblItemName.setBounds(6, 6, 80, 16);
-		contentPane.add(lblItemName);
-		
-		lblItemPrice = new JLabel("Item Price:");
-		lblItemPrice.setBounds(6, 63, 80, 16);
-		contentPane.add(lblItemPrice);
-		
-		lbldisplay = new JLabel("");
-		lbldisplay.setBounds(6, 129, 274, 45);
-		contentPane.add(lbldisplay);
-		
-		rdbtnDarkMode = new JRadioButton("Dark Mode");
-		rdbtnDarkMode.setBounds(6, 198, 141, 23);
-		rdbtnDarkMode.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e){
-				
-			}
-		});
-		contentPane.add(rdbtnDarkMode);
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[]{274, 141, 0};
+		gbl_contentPane.rowHeights = new int[]{16, 26, 16, 26, 45, 44, 47, 0};
+		gbl_contentPane.columnWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		contentPane.setLayout(gbl_contentPane);
 		
 		btnAddItem = new JButton("Add Item");
-		btnAddItem.setBounds(6, 227, 272, 45);
 		btnAddItem.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){
 				String vowels = "aeiou";
@@ -157,7 +122,93 @@ public class Main extends JFrame {
 				contentPane.repaint();
 			}
 		});
-		contentPane.add(btnAddItem);
+		
+		lblItemName = new JLabel("Item Name:");
+		GridBagConstraints gbc_lblItemName = new GridBagConstraints();
+		gbc_lblItemName.anchor = GridBagConstraints.NORTHWEST;
+		gbc_lblItemName.insets = new Insets(0, 0, 5, 5);
+		gbc_lblItemName.gridx = 0;
+		gbc_lblItemName.gridy = 0;
+		contentPane.add(lblItemName, gbc_lblItemName);
+		
+		lblInventory = new JLabel("Inventory:");
+		GridBagConstraints gbc_lblInventory = new GridBagConstraints();
+		gbc_lblInventory.anchor = GridBagConstraints.NORTH;
+		gbc_lblInventory.fill = GridBagConstraints.HORIZONTAL;
+		gbc_lblInventory.insets = new Insets(0, 0, 5, 0);
+		gbc_lblInventory.gridx = 1;
+		gbc_lblInventory.gridy = 0;
+		contentPane.add(lblInventory, gbc_lblInventory);
+		
+		nameTextField = new JTextField();
+		GridBagConstraints gbc_nameTextField = new GridBagConstraints();
+		gbc_nameTextField.anchor = GridBagConstraints.NORTH;
+		gbc_nameTextField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_nameTextField.insets = new Insets(0, 0, 5, 5);
+		gbc_nameTextField.gridx = 0;
+		gbc_nameTextField.gridy = 1;
+		contentPane.add(nameTextField, gbc_nameTextField);
+		nameTextField.setColumns(10);
+		
+		scrollPanel = new JPanel();
+		scrollPanel.setPreferredSize(new Dimension(141, 1000));
+		scrollPanel.setLayout(null);
+		
+		scrollPane = new JScrollPane();
+		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		scrollPane.setViewportView(scrollPanel);
+		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
+		gbc_scrollPane.fill = GridBagConstraints.BOTH;
+		gbc_scrollPane.gridheight = 6;
+		gbc_scrollPane.gridx = 1;
+		gbc_scrollPane.gridy = 1;
+		contentPane.add(scrollPane, gbc_scrollPane);
+		
+		rdbtnDarkMode = new JRadioButton("Dark Mode");
+		rdbtnDarkMode.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				
+			}
+		});
+		
+		lblItemPrice = new JLabel("Item Price:");
+		GridBagConstraints gbc_lblItemPrice = new GridBagConstraints();
+		gbc_lblItemPrice.anchor = GridBagConstraints.NORTHWEST;
+		gbc_lblItemPrice.insets = new Insets(0, 0, 5, 5);
+		gbc_lblItemPrice.gridx = 0;
+		gbc_lblItemPrice.gridy = 2;
+		contentPane.add(lblItemPrice, gbc_lblItemPrice);
+		
+		priceTextField = new JTextField();
+		GridBagConstraints gbc_priceTextField = new GridBagConstraints();
+		gbc_priceTextField.anchor = GridBagConstraints.NORTH;
+		gbc_priceTextField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_priceTextField.insets = new Insets(0, 0, 5, 5);
+		gbc_priceTextField.gridx = 0;
+		gbc_priceTextField.gridy = 3;
+		contentPane.add(priceTextField, gbc_priceTextField);
+		priceTextField.setColumns(10);
+		
+		lbldisplay = new JLabel("");
+		GridBagConstraints gbc_lbldisplay = new GridBagConstraints();
+		gbc_lbldisplay.fill = GridBagConstraints.BOTH;
+		gbc_lbldisplay.insets = new Insets(0, 0, 5, 5);
+		gbc_lbldisplay.gridx = 0;
+		gbc_lbldisplay.gridy = 4;
+		contentPane.add(lbldisplay, gbc_lbldisplay);
+		GridBagConstraints gbc_rdbtnDarkMode = new GridBagConstraints();
+		gbc_rdbtnDarkMode.anchor = GridBagConstraints.NORTHWEST;
+		gbc_rdbtnDarkMode.insets = new Insets(0, 0, 5, 5);
+		gbc_rdbtnDarkMode.gridx = 0;
+		gbc_rdbtnDarkMode.gridy = 5;
+		contentPane.add(rdbtnDarkMode, gbc_rdbtnDarkMode);
+		GridBagConstraints gbc_btnAddItem = new GridBagConstraints();
+		gbc_btnAddItem.anchor = GridBagConstraints.SOUTH;
+		gbc_btnAddItem.fill = GridBagConstraints.HORIZONTAL;
+		gbc_btnAddItem.insets = new Insets(0, 0, 0, 5);
+		gbc_btnAddItem.gridx = 0;
+		gbc_btnAddItem.gridy = 6;
+		contentPane.add(btnAddItem, gbc_btnAddItem);
 		
 		contentPane.getRootPane().setDefaultButton(btnAddItem);
 		
